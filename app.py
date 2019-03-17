adfa1f6ffa368c340af06717c8ed899a1271ade2
hellorobo <trebor.oknewokaj@gmail.com>
2019-03-17 13:18:46 +0100

    soup corrections

--- app.py
+++ app.py
@@ -66,54 +66,49 @@ if isPageLoaded:
     if debug_on: print(dotd_image_src)
     # image_src = dotd_image.a.img['src']
     # image_src = dotd_image.a.img.get('src')
-
     # dotd_txt = soup.find("div", class_="dotd-main-book-summary float-left").select("div")[2].text.strip()
-
     # dotd_image = soup.find("div", class_="dotd-main-book-image float-left")
-
-
     # print("BS parse result:{}".format(dotd))
 
 
-    subject = f'{dotd_title} - Pact Publishing Book of The Day!'
+    subject = f'\"{dotd_title} - Pact Publishing Book of The Day!\"'
     #message = "Today\'s Pact Book: {}\n URL: {}".format(dotd,url)
-
-    css = '''
-    <style type="text/css">
-    body {font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;}
-    h2 {clear: both;font-size: 130%; }
-    h3 {clear: both;font-size: 115%;margin-left: 20px;margin-top: 30px;}
-    p {margin-left: 20px; font-size: 12px;}
-    </style>
-    '''
-
-    html_head = '''
-    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
-    <html><head><title>{}</title>
-    {}
-    </head>
-    '''.format(subject,css)
 
     html_body = '''
     <body>
-
     <h2>Today\'s Pact Book: {0}\n</h2>
-
     <p>
-    <a href="{2}"><img border="0" alt="DOTD" src="{3}" width="224" height="276"></a>
+    <a href=\"{2}\"><img border=\"0\" alt=\"DOTD\" src=\"{3}\" width=\"224\" height=\"276\"></a>
     </p>
+    <p></p>
     <p>author: {1}</p>
     <h3> {2} </h3>
     </body>
     '''.format(dotd_title,dotd_author,url,dotd_image_src)
-
-    html_foot = '</html>'
-
-    message = html_head + html_body + html_foot
-
 else:
-    message = 'Couldn\'t retrieve page contents'
+    html_body = f'<body><h2>Couldn\'t retrieve {url} contents</h2>''
     subject = 'Oh, no! Couldn\'t retrieve today\'s Ebook of the day from Pact Publishing'
+
+css = '''
+<style type=\"text/css\">
+body {font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;}
+h2 {clear: both;font-size: 130%; }
+h3 {clear: both;font-size: 115%;margin-left: 20px;margin-top: 30px;}
+p {margin-left: 20px; font-size: 12px;}
+</style>
+'''
+
+html_head = '''
+<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\">
+<html><head><title>{}</title>
+{}
+</head>
+'''.format(subject,css)
+
+html_foot = '</html>'
+
+message = f'\"{html_head}{html_body}{html_foot}\"'
+
 
 if debug_on: print(message)
 
@@ -125,18 +120,17 @@ if debug_on: print(message)
 API_KEY = os.environ['MJ_APIKEY_PUBLIC']
 API_SECRET = os.environ['MJ_APIKEY_PRIVATE']
 
-mailjet = Client(auth=(API_KEY, API_SECRET), version='v3')
-fromname = 'MessangerAPI'
-sender = 'messanger@tutamail.com'
-recipients = [{'Email': 'ignorethismessage@gmail.com'}]
-
+mailjet = Client(auth=(API_KEY, API_SECRET), version='v3.1')
+mail_from = {"Email": "messanger@tutamail.com", "Name": "MessangerAPI"}
+mail_to = [{"Email": "ignorethismessage@gmail.com"}]
+mail_subject = subject
+mail_htmlpart = message
 
 email = {
-    'FromName': fromname,
-    'FromEmail': sender,
-    'Subject': subject,
-    'Html-Part': message,
-    'Recipients': recipients
+    "From": mail_from,
+    "To": mail_to,
+    "Subject": mail_subject,
+    "Html-Part": mail_htmlpart
 }
 
 response = mailjet.send.create(email)
